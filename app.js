ee5b6fc3a09f52380d2c47e13e5572743adde5f1
uzair-675 <uzairsaeed2015@gmail.com>
2024-03-10 15:36:22 +0500

    ---

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ async function startServer() {
 
   await loaders({ expressApp: app });
 
-  // new
+
   app.listen(config.port, () => {
     logger.info(`Server is listening on port: ${config.port}`);
   }).on('error', err => {
